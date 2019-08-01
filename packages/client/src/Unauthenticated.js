bcb814711e113c27d3e23c0a754a705b651eea34
kriswep <kriswep@wetainment.com>
2019-08-01 21:36:29 +0200

    add side to larger home

--- packages/client/src/Unauthenticated.js
+++ packages/client/src/Unauthenticated.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, ImageBackground } from 'react-native';
 
 import Text from './Text';
 import { useSizeState } from './Size';
 import { useAuthState } from './auth/authWrapper';
+import Side from './Side';
 
 const Unauthenticated = () => {
   const [auth, isAuthenticated] = useAuthState();
@@ -41,106 +42,129 @@ const Unauthenticated = () => {
       </View>
       <View
         css={`
-          align-self: stretch;
-          align-items: center;
-          justify-content: center;
+          flex-direction: row;
+          flex: 1;
         `}
       >
-        <Text
-          dim={dimension}
+        <View
           css={`
-            text-align: center;
-            color: ${props => props.theme.colors.white};
-            font-size: ${props => props.theme.fontSizes[4]}px;
-            padding: ${props => props.theme.space[5]}px
-              ${props => props.theme.space[3]}px;
-            ${({ dim, theme }) =>
-              dim.m &&
-              `
-              font-size: ${theme.fontSizes[5]}px;
-              padding: ${theme.space[6]}px ${theme.space[4]}px;
-            `}
-          `}
-        >
-          Manage your daily grocery shopping with ease.
-        </Text>
-        <TouchableOpacity
-          accessibilityLabel="Log In"
-          dim={dimension}
-          css={`
+            flex: 1;
+            align-self: stretch;
             align-items: center;
-            border-radius: ${props => props.theme.space[4]}px;
-            background-color: ${props => props.theme.colors.white};
-            margin-bottom: ${props => props.theme.space[4]}px;
-            padding: ${props => props.theme.space[2]}px
-              ${props => props.theme.space[3]}px;
-            min-width: ${props => props.theme.sizes[6]}px;
-            ${({ dim, theme }) =>
-              dim.m &&
-              `
-              min-width: ${theme.sizes[7]}px;
-              padding: ${theme.space[3]}px;
-              margin-bottom: ${theme.space[5]}px;
-            `}
+            justify-content: center;
           `}
-          onPress={() => auth.login()}
         >
           <Text
             dim={dimension}
             css={`
-              color: ${props => props.theme.colors.black};
-              font-family: nunito-bold;
-              font-size: ${props => props.theme.fontSizes[3]}px;
-              font-weight: 700;
-              ${({ dim, theme }) =>
-                dim.m &&
-                `
-                font-size: ${theme.fontSizes[4]}px;
-              `}
-            `}
-          >
-            Log In
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          accessibilityLabel="Sign Up"
-          dim={dimension}
-          css={`
-            align-items: center;
-            border-radius: ${props => props.theme.space[4]}px;
-            background-color: ${props => props.theme.colors.black};
-            border: solid ${props => props.theme.colors.white}
-              ${props => props.theme.sizes.fine}px;
-            margin-bottom: ${props => props.theme.space[3]}px;
-            padding: ${props => props.theme.space[2]}px
-              ${props => props.theme.space[3]}px;
-            min-width: ${props => props.theme.sizes[6]}px;
-            ${({ dim, theme }) =>
-              dim.m &&
-              `
-              min-width: ${theme.sizes[7]}px;
-              padding: ${theme.space[3]}px;
-            `}
-          `}
-          onPress={() => auth.login(true)}
-        >
-          <Text
-            dim={dimension}
-            css={`
+              text-align: center;
               color: ${props => props.theme.colors.white};
-              font-family: nunito;
-              font-size: ${props => props.theme.fontSizes[3]}px;
-              font-weight: 400;
+              font-size: ${props => props.theme.fontSizes[4]}px;
+              padding: ${props => props.theme.space[5]}px
+                ${props => props.theme.space[3]}px;
               ${({ dim, theme }) =>
                 dim.m &&
                 `
-                font-size: ${theme.fontSizes[4]}px;
+                font-size: ${theme.fontSizes[5]}px;
+                padding: ${theme.space[6]}px ${theme.space[4]}px;
               `}
             `}
           >
-            Sign Up
+            Manage your daily grocery shopping with ease.
           </Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            accessibilityLabel="Log In"
+            dim={dimension}
+            css={`
+              align-items: center;
+              border-radius: ${props => props.theme.space[4]}px;
+              background-color: ${props => props.theme.colors.white};
+              margin-bottom: ${props => props.theme.space[4]}px;
+              padding: ${props => props.theme.space[2]}px
+                ${props => props.theme.space[3]}px;
+              min-width: ${props => props.theme.sizes[6]}px;
+              ${({ dim, theme }) =>
+                dim.m &&
+                `
+                min-width: ${theme.sizes[7]}px;
+                padding: ${theme.space[3]}px;
+                margin-bottom: ${theme.space[5]}px;
+              `}
+            `}
+            onPress={() => auth.login()}
+          >
+            <Text
+              dim={dimension}
+              css={`
+                color: ${props => props.theme.colors.black};
+                font-family: nunito-bold;
+                font-size: ${props => props.theme.fontSizes[3]}px;
+                font-weight: 700;
+                ${({ dim, theme }) =>
+                  dim.m &&
+                  `
+                  font-size: ${theme.fontSizes[4]}px;
+                `}
+              `}
+            >
+              Log In
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            accessibilityLabel="Sign Up"
+            dim={dimension}
+            css={`
+              align-items: center;
+              border-radius: ${props => props.theme.space[4]}px;
+              background-color: ${props => props.theme.colors.black};
+              border: solid ${props => props.theme.colors.white}
+                ${props => props.theme.sizes.fine}px;
+              margin-bottom: ${props => props.theme.space[3]}px;
+              padding: ${props => props.theme.space[2]}px
+                ${props => props.theme.space[3]}px;
+              min-width: ${props => props.theme.sizes[6]}px;
+              ${({ dim, theme }) =>
+                dim.m &&
+                `
+                min-width: ${theme.sizes[7]}px;
+                padding: ${theme.space[3]}px;
+              `}
+            `}
+            onPress={() => auth.login(true)}
+          >
+            <Text
+              dim={dimension}
+              css={`
+                color: ${props => props.theme.colors.white};
+                font-family: nunito;
+                font-size: ${props => props.theme.fontSizes[3]}px;
+                font-weight: 400;
+                ${({ dim, theme }) =>
+                  dim.m &&
+                  `
+                  font-size: ${theme.fontSizes[4]}px;
+                `}
+              `}
+            >
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {dimension.l && (
+          <View
+            dim={dimension}
+            css={`
+              flex: 1;
+              ${({ dim }) =>
+                dim.xl &&
+                `
+                flex: 1;
+              `};
+            `}
+          >
+            <Side />
+          </View>
+        )}
       </View>
     </>
   );
